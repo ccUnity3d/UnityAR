f7c2c44d1fbb6bdc9bb5b0e10c7896141dfdad6f
van01 <van_01@hotmail.com>
2015-03-25 18:24:14 +0900

    ui 변경

---
+++ UI_CanvasTest/Assets/Script/UI_2D_Custem_Rotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_2D_Custem_Rotate : MonoBehaviour {
+
+	public float m_fSpeed = 30;
+	private RectTransform m_trans;
+	
+	// Use this for initialization
+	void Start () {
+		m_trans = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Quaternion quater = m_trans.localRotation;
+		Vector3 rot = quater.eulerAngles;
+		quater = Quaternion.Euler(rot.x, rot.y , rot.z + m_fSpeed * Time.deltaTime);
+		m_trans.localRotation = quater;
+	}
+	
+	public void setSpeed(float fSpeed)
+	{
+		m_fSpeed = fSpeed;
+	}
+}
---
+++ UI_CanvasTest/Assets/Script/UI_SideCircle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_SideCircle : MonoBehaviour {
+
+	public GameObject m_objCircle1;
+	public GameObject m_objCircle2;
+
+	private UI_2D_Custem_Rotate m_circle1;
+	private UI_2D_Custem_Rotate m_circle2;
+
+	private float m_fMaxTime  = 5.0f;
+	private float m_fTime = 0.0f;
+	public float m_fSpeed = 30;
+	private RectTransform m_trans;
+
+	
+	// Use this for initialization
+	void Start () {
+		m_trans = GetComponent<RectTransform> ();
+		m_circle1 = m_objCircle1.GetComponent<UI_2D_Custem_Rotate>();
+		m_circle2 = m_objCircle2.GetComponent<UI_2D_Custem_Rotate>();
+
+		reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		m_fTime += Time.deltaTime;
+		
+		if (m_fTime > m_fMaxTime)
+		{
+			reset();
+		}
+	}
+	
+	void reset()
+	{
+		m_fTime = 0.0f;
+		m_fMaxTime = Random.Range (3, 10);
+		m_fSpeed = Random.Range (-10, 10) * 10;
+
+		m_circle1.setSpeed (m_fSpeed);
+		m_circle2.setSpeed (-m_fSpeed);
+	}
+}
