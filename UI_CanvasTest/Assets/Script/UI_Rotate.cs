df1737900ad22eb90de7490b15ee6cae934c2635
Jogyeongseong <notshade@gmail.com>
2015-03-25 12:42:38 +0900

    Center UI 추가

---
+++ UI_CanvasTest/Assets/Script/UI_Rotate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Rotate : MonoBehaviour {
+	
+	private float m_fMaxTime  = 5.0f;
+	private float m_fTime = 0.0f;
+	public float m_fSpeed = 30;
+
+	// Use this for initialization
+	void Start () {
+		reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Quaternion quater = transform.rotation;
+		Vector3 rot = quater.eulerAngles;
+		quater = Quaternion.Euler(rot.x, rot.y + m_fSpeed * Time.deltaTime, rot.z);
+		transform.rotation = quater;
+
+		m_fTime += Time.deltaTime;
+
+		if (m_fTime > m_fMaxTime)
+		{
+			reset();
+		}
+	}
+
+	void reset()
+	{
+		m_fTime = 0.0f;
+		m_fMaxTime = Random.Range (3, 10);
+		m_fSpeed = Random.Range (-10, 10) * 5;
+	}
+}
